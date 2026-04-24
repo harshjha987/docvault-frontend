81c0aa929c7765d369f83ef1ac16d3c47c1bdf04
harshjha987 <jha.harsh837@gmail.com>
2026-04-24 15:42:16 +0530

    profile section for phone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,6 +139,12 @@ hover:opacity-90 transition"
                   Dashboard
                 </Link>
                 <Link
+                  to="/profile"
+                  className="text-gray-600 dark:text-gray-300 font-medium"
+                >
+                  Profile
+                </Link>
+                <Link
                   to="/folders"
                   className="text-gray-600 dark:text-gray-300 font-medium"
                 >
